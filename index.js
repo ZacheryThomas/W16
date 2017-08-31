b232d079d489ca6e92588124822a39ff1bc52476
Zach Thomas <zacherythomas12@gmail.com>
2017-08-31 19:35:50 -0400

    Renamed objects to element

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 canvas = document.getElementById("whyupdate");
 context = canvas.getContext('2d');
 
-var names = new Array();
-names.push("Wuff");
-names.push("Howl");
 
 var sources = new Array();
 sources.push("https://www.zacherythomas.com/images/eyes.jpg");
 sources.push("http://68.media.tumblr.com/f2dd914736f5fda3746dbb9932e241db/tumblr_ovfijeR9Dy1uojm1ao1_250.jpg");
-sources.push("https://www.zacherythomas.com/images/eyes.jpg")
 
-var pictures = new Array();
-var Objects = []
+var Elements = []
 
 function Sprite(x, y, width, height, src) {
-  this.X = x;
-  this.Y = y;
-  this.image = new Image();
-  this.image.width = width;
-  this.image.height = height;
-  this.image.src = src;
+    this.X = x;
+    this.Y = y;
+    this.image = new Image();
+    this.image.width = width;
+    this.image.height = height;
+    this.image.src = src;
 }
 
-function Object(x, y, z, name, static, sprite){
+function Element(x, y, z, name, static, sprite){
     this.name = name
-  this.static = static
-  this.sprite = sprite
-  this.width = 200
-  this.height = 200
-  this.X = x
-  this.Y = y
-  this.Z = z
+    this.static = static
+    this.sprite = sprite
+    this.width = 200
+    this.height = 200
+    this.X = x
+    this.Y = y
+    this.Z = z
 }
 
 var picture = new Sprite(20, 20, 200, 200, sources[0]);
-var eye = new Object(0, 0, 0, 'eye', false, picture)
-Objects.push(eye)
+var eye = new Element(0, 0, 0, 'eye', false, picture)
+Elements.push(eye)
 
+var picture = new Sprite(20, 20, 200, 200, sources[1]);
+var card = new Element(0, 0, 0, 'eye', false, picture)
+Elements.push(card)
 
 mouseDownToggle = false
 mouseUpToggle = false
@@ -66,8 +64,8 @@ function mouseHandler(){
 
 function checkOverlap(){
     overlapping = []
-    for (obj1 of Objects){
-        for (obj2 of Objects){
+    for (obj1 of Elements){
+        for (obj2 of Elements){
             if (obj1 === obj2){
                 continue
             }
@@ -75,13 +73,13 @@ function checkOverlap(){
             // Enure that leftmost edge of pic1 is less than the furthest right edge of pic2
             // and rightmost edge of pic one is less than the furthest edge of pic2
             // same with height
-            inBoundsX = (obj1.X < obj2.X + obj2.image.width) && (obj1.X + obj1.image.width > obj2.X)
-            inBoundsY = (obj1.Y < obj2.Y + obj2.image.height) && (obj1.Y + obj1.image.height > obj2.Y)
+            inBoundsX = (obj1.X < obj2.X + obj2.width) && (obj1.X + obj1.width > obj2.X)
+            inBoundsY = (obj1.Y < obj2.Y + obj2.height) && (obj1.Y + obj1.height > obj2.Y)
             
             if (inBoundsX && inBoundsY){
                 combo = [obj2, obj1]
 
-                // Make sure that this combo of objects doesnt exist in list already
+                // Make sure that this combo of elements doesnt exist in list already
                 exists = false
                 for (element of overlapping){
                     if (element[1] == combo[0] && element[0] == combo[1]){
@@ -113,34 +111,36 @@ function checkMouseOn(obj, x, y) {
     return false;
 }
 
+
 function dragHandler(){
-    this.drag_object = undefined
+    this.drag_element = undefined
     this.offsetX = 0
     this.offsetY = 0
 
     this.mouseMove = function() {
-        if (this.drag_object) {
-            this.drag_object.X = mousePosition.X - this.offsetX
-            this.drag_object.Y = mousePosition.Y - this.offsetY
+        if (this.drag_element) {
+            this.drag_element.X = mousePosition.X - this.offsetX
+            this.drag_element.Y = mousePosition.Y - this.offsetY
         }
     }
 
     this.mouseDown = function() {
-        for (var iter = 0; iter < Objects.length; iter++) {
-            if (checkMouseOn(Objects[iter], mousePosition.X, mousePosition.Y)) {
-                this.drag_object = Objects[iter]
+        for (var iter = 0; iter < Elements.length; iter++) {
+            if (checkMouseOn(Elements[iter], mousePosition.X, mousePosition.Y)) {
+                this.drag_element = Elements[iter]
 
-                this.offsetX = mousePosition.X - this.drag_object.X
-                this.offsetY = mousePosition.Y - this.drag_object.Y
+                this.offsetX = mousePosition.X - this.drag_element.X
+                this.offsetY = mousePosition.Y - this.drag_element.Y
             }
         }
     }
 
     this.mouseUp = function() {
-        this.drag_object = undefined
+        this.drag_element = undefined
     }
 }
 dHandler = new dragHandler()
+
 
 function update() {
     if (mouseUpToggle){
@@ -166,18 +166,17 @@ function draw() {
     canvas.width = canvas.width;
 
     //draw outline around image w/ focus
-    if (dHandler.drag_object) {
+    if (dHandler.drag_element) {
         context.beginPath();
         context.lineWidth = "6";
         context.strokeStyle = "red";
-        context.rect(dHandler.drag_object.X, dHandler.drag_object.Y, 
-            dHandler.drag_object.width, dHandler.drag_object.height);
+        context.rect(dHandler.drag_element.X, dHandler.drag_element.Y, dHandler.drag_element.width, dHandler.drag_element.height);
         context.stroke();
         
     }
         
-    for (var iter = 0; iter < Objects.length; iter++) {
-        context.drawImage(Objects[iter].sprite.image, Objects[iter].X, Objects[iter].Y, Objects[iter].width, Objects[iter].height);
+    for (var iter = 0; iter < Elements.length; iter++) {
+        context.drawImage(Elements[iter].sprite.image, Elements[iter].X, Elements[iter].Y, Elements[iter].width, Elements[iter].height);
 
     }
 
